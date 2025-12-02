21d5b1ed0d13dcd5fed6caad60e1561a01b839bc
gushumani-rgb <gushumani@gmail.com>
2025-12-02 19:00:42 +0200

    Create onesignal-init.js

---
+++ onesignal-init.js
@@ -0,0 +1,10 @@
+// onesignal-init.js
+window.OneSignal = window.OneSignal || [];
+OneSignal.push(function() {
+    OneSignal.init({
+        appId: "89047e79-1315-495f-8a89-5e7c7f29a31a",
+        notifyButton: {
+            enable: true
+        }
+    });
+});
