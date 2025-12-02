9dfc27e9e96d5a4265130e8ceebcf2826d33a653
gushumani-rgb <gushumani@gmail.com>
2025-12-02 15:36:27 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (event) => {
-console.log('Service Worker installing.');
-// You can add caching here if needed
-});
-
-self.addEventListener('fetch', (event) => {
-// For now, just pass requests through
-event.respondWith(fetch(event.request));
-});
