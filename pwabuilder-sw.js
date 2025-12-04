8ca41ef660dd54fd2571a72d86f12b9d2a6be59d
gushumani-rgb <gushumani@gmail.com>
2025-12-04 14:50:05 +0200

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,50 +1,37 @@
-// Ask user permission for notifications
-Notification.requestPermission().then(permission => {
-  if (permission === 'granted') {
-    console.log('Notification permission granted.');
-  } else {
-    console.log('Notification permission denied.');
-  }
+const CACHE_NAME = 'cool-cache';
+
+// Add whichever assets you want to precache here:
+const PRECACHE_ASSETS = [
+    '/assets/',
+    '/src/'
+]
+
+// Listener for the install event - precaches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
 });
 
-// Listen for push events
-self.addEventListener('push', (event) => {
-  let data = {};
-  if (event.data) {
-    data = event.data.json();
-  }
-
-  const title = data.title || 'Notification Title';
-  const options = {
-    body: data.body || 'Notification Body Text',
-    icon: data.icon || 'custom-notification-icon.png',
-    data: {
-      path: data.path || '/', // path to open on click
-    },
-  };
-
-  event.waitUntil(self.registration.showNotification(title, options));
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 
-// Handle notification clicks
-self.addEventListener('notificationclick', (event) => {
-  event.notification.close();
-  const notificationData = event.notification.data;
+self.addEventListener('fetch', event => {
+  event.respondWith(async () => {
+      const cache = await caches.open(CACHE_NAME);
 
-  const urlToOpen = new URL(notificationData.path, self.location.origin).href;
+      // match the request to our cache
+      const cachedResponse = await cache.match(event.request);
 
-  event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
-      // If a window is already open, focus it
-      for (const client of windowClients) {
-        if (client.url === urlToOpen && 'focus' in client) {
-          return client.focus();
-        }
-      }
-      // Otherwise, open a new window
-      if (clients.openWindow) {
-        return clients.openWindow(urlToOpen);
-      }
-    })
-  );
+      // check if we got a valid response
+      if (cachedResponse !== undefined) {
+          // Cache hit, return the resource
+          return cachedResponse;
+      } else {
+        // Otherwise, go to the network
+          return fetch(event.request)
+      };
+  });
 });
